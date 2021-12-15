3a8055ad27241029a1a01eab748057f8eff602b3
bakano98 <70756909+bakano98@users.noreply.github.com>
2021-12-15 18:16:56 +0800

    appt system complete
    UI/UX improvements + integration to main app left

--- zoomTest/App.js
+++ zoomTest/App.js
@@ -2,19 +2,26 @@
 // Need to add an option to turn off notifications
 
 import React, { useState, useEffect } from 'react';
-import { Modal, TouchableOpacity, StyleSheet, Text, View, Pressable } from 'react-native';
+import { Modal, TouchableOpacity, StyleSheet, Text, View, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { Calendar } from './Custom';
+import { Touchable } from 'react-native-web';
+
 // Use modal with our custom calendar. For our purpose, we have to:
 // 1) Block out dates that have been booked
 // As a mockup, we'll do it as such: block out the entire date if it has been booked. Then we'll have to explain
 // 2) Block out weekends
 // Should be trivial -- simply need to check if COL === SAT/SUN, then check if date has been booked.
+// Lastly, when 
 
 // Need AsyncStorage to store dates that have been picked.
 const App = () =>  {
   // Define your time with hooks. We use this as the default time. This is a string used to display time only.
   const [time, setTime] = useState("12:34 PM");
+  const [apptDate, setApptDate] = useState('NONE');
+  const getChosenDateFromCalendar = (chosenDate) => {
+    setApptDate(chosenDate); // callback function. Works properly, but not optimal. May want to refactor after everything is done.
+  }
   // Modal
   const [modalVisible, setModalVisible] = useState(false);
   const modal = () => {
@@ -24,26 +31,26 @@ const App = () =>  {
         animationType="slide"
         transparent={true}
         visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert("Modal has been closed.");
-          setModalVisible(!modalVisible);
-        }}
       >
         <View>
           <View style={styles.modalView}>
-            <Text style={styles.modalText}>Pick an appointment date</Text>
-            <Calendar width="100%" height="45%"/>
+            <Text style={{fontSize: 30, fontWeight: 'bold', textDecorationLine: 'underline'}}>Pick an appointment date</Text>
+            <Calendar 
+              width="100%" 
+              height="45%" 
+              getDate={getChosenDateFromCalendar} /* callback function. Will probably need to be refactored next time */
+            /> 
             <TouchableOpacity
-              style={{position: 'absolute', bottom: 0, marginBottom: 100}}
+              style={{position: 'absolute', bottom: 0, margin: 100}}
               onPress={() => setModalVisible(!modalVisible)}
             >
-              <Text style={{color: 'red', fontSize: 25}}>Confirm</Text>
+              <Text style={styles.confirm}>Select</Text>
             </TouchableOpacity>
           </View>
         </View>
       </Modal>
       <TouchableOpacity onPress={() => setModalVisible(true)}>
-        <Text style={styles.text}>Show Modal</Text>
+        <Text style={styles.text}>Select Dates</Text>
       </TouchableOpacity>
     </View>
     )
@@ -77,21 +84,18 @@ const App = () =>  {
     showMode('time');
   };
 
+  useEffect(() => {
 
-  // <------------------------------------ Notifications logic  ------------------------------------->
+  }, [time, apptDate])
+
   const split_time = time.split(':'); // split by ':', because the hour and minute are separated that way
   const AM_PM = split_time[1].split(" ")[1];
   const hrs = Number(split_time[0]);
   const mins = Number(split_time[1].split(" ")[0]);
   // remove comment if you want to do some debugging
-  // console.log(time);
-  console.log(date);
+  // console.log(date);
   return (
     <View style={styles.container}>
-      <View style={{flexDirection: 'row'}}>
-        <Text>Date: </Text>
-        <Text>Time: {time} </Text>
-      </View>
       <View style={{flexDirection: 'row'}}>
           {modal()}
         <View style={styles.timeContainer}>
@@ -111,6 +115,17 @@ const App = () =>  {
             />
           ) /* Can just ignore this entire thing. It's simply used to help open up the time picker*/}
         </View>
+      </View>
+      <View style={{position: 'absolute', top: 100}}>
+        <Text style={styles.apptText}>Current Appointment Details</Text>
+        <Text style={styles.apptText}>Date: {apptDate}</Text>
+        <Text style={styles.apptText}>Time: {time}</Text>
+      </View>
+      <View style={{position: 'absolute', bottom: 100}}>
+        <TouchableOpacity onPress={() => Alert.alert('Request sent',
+        `Requested for an appointment on ${apptDate}, at ${time}`)}>
+          <Text style={styles.confirm}>Confirm</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -133,7 +148,12 @@ const styles = StyleSheet.create({
     width: 220, 
     alignItems: 'center', 
     justifyContent: 'center',
-    marginRight: 30
+    marginLeft: 15,
+    marginRight: 15,
+  },
+  confirm: {
+    color: 'red', 
+    fontSize: 25
   },
 
   text: {
@@ -141,6 +161,13 @@ const styles = StyleSheet.create({
     color: '#4169e1',
     fontWeight: 'bold'
   },
+
+  apptText: {
+    fontSize: 24,
+    color:'black',
+    fontWeight: 'bold'
+  },
+
   centeredView: {
     flex: 1,
     justifyContent: "center",
--- zoomTest/Custom/Calendar.js
+++ zoomTest/Custom/Calendar.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { SafeAreaView, Text, Button, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, Text, Button, View, Alert} from 'react-native';
 import * as dateFn from 'date-fns';
 
 const todayDate = new Date();
@@ -8,7 +8,7 @@ const dateToCompare = new Date(todayDate.getFullYear(), todayDate.getMonth(), 1)
 const Calendar = (props) => {
   // hooks to set dates
   const [date, setDate] = useState(new Date());
-
+  const [chosen, setChosen] = useState("NONE");
 
   // months in calendar
   const months = ["January", "February", "March", "April", 
@@ -93,7 +93,7 @@ const Calendar = (props) => {
     
     // actual populationdateToCompare
     // styles functions
-    const colourPicker = (item, rowIndex, colIndex) => { 
+    const fontColourPicker = (item, rowIndex, colIndex) => { 
       return rowIndex === 0 
               ? colIndex === 0
                 ? 'red'
@@ -105,29 +105,31 @@ const Calendar = (props) => {
 
     const bgColourPicker = (rowIndex, item) => {
       return rowIndex === 0 
-              ? '#ddd' 
-              : item.isBooked
-                ? 'red'
-                : '#fff'
+              ? '#ddd'
+              : item.value === chosen
+                ? '#90EE90'
+                : item.isBooked
+                  ? 'red'
+                  : '#fff'
     }
 
+    // generating the entire calendar's items
     rows = matrix.map((row, rowIndex) => {
       let rowItems = row.map((item, colIndex) => {
         return (
           <Text
             style={{
               flex: 1,
-              height: 20,
+              height: 30,
               textAlign: 'center',
               // Highlight header
               backgroundColor: bgColourPicker(rowIndex, item),
               // Highlight Sundays
-              color: colourPicker(item, rowIndex, colIndex),
+              color: fontColourPicker(item, rowIndex, colIndex),
               // Highlight current date
-              fontWeight: item == date.getDate() 
+              fontWeight: item.value === date.getDate() 
                                   ? 'bold': 'normal',
               fontSize: 18,
-              
             }}
             onPress={() => pressCalendar(item, colIndex) /* Needs to be changed to go to mood selection screen */}>
             {item.value != -1 ? item.value : ''}
@@ -148,33 +150,38 @@ const Calendar = (props) => {
       );
     });
     
+    // happens when you click on an object in the calendar
     const pressCalendar = (item, col) => {
       if (item === '') {
         // then do nothing
-      } else if (col === 0 || col === 6) {
-        alert("Unable to book on weekends");
-      } else if (item.isBooked) {
-        alert("Fully booked");
       } else if (item.value <= todayDate.getDate()) {
-        alert()
-      } else {
+        Alert.alert("Invalid date", "Only able to book appointments from tomorrow onwards")
+      } else if (col === 0 || col === 6) {
+        Alert.alert("Unavailable", "Sorry, unable to book on weekends");
+      } else if (item.isBooked) {
+        Alert.alert("Fully booked", "Please choose another date");
+      }  else {
         // valid, so we'll need to retrieve the date then black it out..
-        console.log('This is ok');
-        console.log(todayDate.getDate());
+        setChosen(item.value);
+        const year = date.getFullYear();
+        const month = date.getMonth()+1;
+        const day = item.value;
+        props.getDate(day + "-" + month + "-" + year);
       }
     }
     
     
+    // changes the month on screen
     const changeMonth = (n) => {
+      setChosen(0); // to reset the chosen date
       const curr = n > 0 ? dateFn.addMonths(date, Math.abs(n)) : dateFn.subMonths(date, Math.abs(n));
       if (curr <= dateToCompare) {
-        alert("Unable to book an appointment before today.")
+        Alert.alert("Invalid","Unable to book an appointment before today.")
       } else if (curr > dateFn.addMonths(dateToCompare, Math.abs(6))) {
-        alert("Unable to book too far in the future")
+        Alert.alert("Unavailable","Unable to book too far in the future")
       } else {
         return setDate(curr);
       }
-      console.log(todayDate);
     }
     
 
